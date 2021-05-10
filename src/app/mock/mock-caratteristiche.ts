faf19f8dff6c7d37b66c3aa9d796aa174a73ef6b
andrea_falivene <andrea.falivene@gmail.com>
2021-05-10 18:56:58 +0200

    init

---
+++ src/app/mock/mock-caratteristiche.ts
@@ -0,0 +1,11 @@
+import { Caratteristiche } from "../model/caratteristiche";
+
+export const MOCK_MESSAGES: Caratteristiche[] = [
+
+     {id:1,nome:'superman',eta:35,sesso:true,segniParticolari:'vola',note:''},
+     {id:2,nome:'hulk',eta:25,sesso:false,segniParticolari:'super forza',note:''},
+    
+
+    
+  
+];
